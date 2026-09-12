9d904a35614923af175bf0e5b98f121f4c81ca5f
binarybrain-7 <aadivish1606@gmail.com>
2026-09-13 04:08:29 +0530

    Time: 13 ms (26.56%) | Memory: 64 MB (9.98%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null|| head.next==null){
+        return head;
+        }   
+           ListNode temp = head;
+      int count =0;
+      while(temp!=null){
+        count++;
+        temp=temp.next;
+
+      }
+      temp =head;
+      int arr[] = new int[count];
+      int  i=0;
+ while(temp!=null){
+        arr[i]=temp.val;
+        i++;
+        temp=temp.next;
+
+      }
+      Arrays.sort(arr);
+      temp=head;
+      int j =0;
+      while(temp!=null){
+        temp.val=arr[j];
+        j++;
+        temp=temp.next;
+      }
+        return head;
+    }
+}
