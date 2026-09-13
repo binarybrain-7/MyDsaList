aac2acbc7a0244ae07cd75a39f25629a97802e79
binarybrain-7 <aadivish1606@gmail.com>
2026-09-13 16:45:58 +0530

    Time: 10 ms (49.36%) | Memory: 59.6 MB (30.56%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,34 +9,51 @@
  * }
  */
 class Solution {
+    private static ListNode middle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyList = new ListNode(-1, null);
+        ListNode temp = dummyList;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+        return dummyList.next;
+
+    }
+
     public ListNode sortList(ListNode head) {
-        if(head==null|| head.next==null){
-        return head;
-        }   
-           ListNode temp = head;
-      int count =0;
-      while(temp!=null){
-        count++;
-        temp=temp.next;
+        if (head == null || head.next == null) {
+            return head;
+        }
 
-      }
-      temp =head;
-      int arr[] = new int[count];
-      int  i=0;
- while(temp!=null){
-        arr[i]=temp.val;
-        i++;
-        temp=temp.next;
+        ListNode mid = middle(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
 
-      }
-      Arrays.sort(arr);
-      temp=head;
-      int j =0;
-      while(temp!=null){
-        temp.val=arr[j];
-        j++;
-        temp=temp.next;
-      }
-        return head;
     }
 }
