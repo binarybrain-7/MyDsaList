7cd6f72515bcc9ca5d9303c841edfc5002253606
binarybrain-7 <aadivish1606@gmail.com>
2026-09-14 12:53:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (33.30%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -7,5 +7,12 @@ class Solution {
         boolean yOverlap = Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
         
         return xOverlap && yOverlap;
+        // if (rec1[2] <= rec2[0] || // rec1 is left of rec2
+        //     rec1[0] >= rec2[2] || // rec1 is right of rec2
+        //     rec1[3] <= rec2[1] || // rec1 is below rec2
+        //     rec1[1] >= rec2[3]) { // rec1 is above rec2
+        //     return false;
+        // }
+        // return true;
     }
 }
